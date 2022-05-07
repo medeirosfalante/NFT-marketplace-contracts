beb6299802f929521a77daf184b6cf4b034accaa
Medeirosfalante <rafaelgurudesign@gmail.com>
2022-05-06 23:22:39 -0300

    remove console.log accounts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,7 +7,6 @@ module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(ArtMarketplace)
   const market = await ArtMarketplace.deployed()
   const instance = await deployer.deploy(NFT, 'Non Fungable Tokens', 'NFT')
-  console.log(accounts[0])
   const tokenIntance = await deployer.deploy(Token, 'BRZ', 100000000000, {
     from: accounts[0],
   })
