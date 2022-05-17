8184d9fd2712265dc16e01d43fdbb0edf71fffd3
Medeirosfalante <rafaelgurudesign@gmail.com>
2022-05-17 01:35:03 -0300

    configure time network testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,23 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const fs = require("fs");
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const fs = require('fs')
+const mnemonic = fs.readFileSync('.secret').toString().trim()
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1", // Localhost (default: none)
+      host: '127.0.0.1', // Localhost (default: none)
       port: 7545, // Standard BSC port (default: none)
-      network_id: "*", // Any network (default: none)
+      network_id: '*', // Any network (default: none)
     },
     testnet: {
       provider: () =>
         new HDWalletProvider(
           mnemonic,
-          `https://data-seed-prebsc-2-s2.binance.org:8545`
+          `https://data-seed-prebsc-1-s1.binance.org:8545`,
         ),
       network_id: 97,
-      confirmations: 10,
-      timeoutBlocks: 200,
+      confirmations: 2,
+      timeoutBlocks: 2000,
       skipDryRun: true,
       networkCheckTimeout: 1000000,
     },
@@ -39,14 +39,14 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.13", // A version or constraint - Ex. "^0.8.0"
+      version: '0.8.13', // A version or constraint - Ex. "^0.8.0"
       settings: {
         optimizer: {
           enabled: true,
           runs: 2,
         },
-        evmVersion: "byzantium"
+        evmVersion: 'byzantium',
       },
     },
   },
-};
+}
